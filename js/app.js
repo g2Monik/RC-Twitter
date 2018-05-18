3980b826c8ba69914256d565cf271b0b5d29a30f
g2Monik <g2monik@gmail.com>
2018-05-17 22:32:37 -0600

    Version 0.0.2 deel reto Twitter

--- js/app.js
+++ js/app.js
@@ -10,12 +10,25 @@ button.addEventListener('click', function(){
 	//Div para comentarios
 	var newComments = document.createElement('div');
 	//sin mensajes vacios
+  if(comments.length == 0 || comments == null) {
+		document.getElementById("button").disabled = true;
+	}
 
+   document.getElementById('comment').onkeyup = function () {
+   document.getElementById('count').innerHTML = "Characters left: " + (140 - this.value.length);
+  };
 	var textNewComment = document.createTextNode(comments);
 	var contenedorElemento = document.createElement('p');
 	contenedorElemento.appendChild(textNewComment);
 	newComments.appendChild(contenedorElemento);
-
 	cont.appendChild(newComments);
-
 })
+
+var caracteres = document.getElementById("caracteres");
+var mensaje = document.getElementById("comment");
+var cont = 140;
+caracteres.innerHTML = cont;
+function cambiaMensaje(evento) {
+cont = 140 - evento.target.value.length;
+  caracteres.innerHTML = cont;
+}
